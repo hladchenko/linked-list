985bb7dea3babcbba944da66101bced660f119c4
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-04 17:18:56 +0300

    init

---
+++ LinkedList.py
@@ -0,0 +1,24 @@
+from Node import Node
+
+class LinkedList:
+  size = 0
+
+  def __init__(self, element):
+    self.root = Node(element)
+    self.size = 1
+
+  def add(self, element):
+    self._add(self.root, element)
+
+  def _add(self, root, element):
+    if not root.next:
+      root.next = Node(element)
+    else:
+      self._add(root.next, element)
+    self.size += 1;
+
+  def print(self):
+    self._print(self.root)
+
+  def print(self, root):
+    print("TODO")
---
+++ Main.py
@@ -0,0 +1,10 @@
+from LinkedList import LinkedList
+from Node import Node
+
+list = LinkedList(1)
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+print(list.root.element)
